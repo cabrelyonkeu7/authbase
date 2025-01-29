ceb7158004940612a133fd1f1d5cda8d36842df7
Cabrel Yonkeu <yonkeucabrel@gmail.com>
2025-01-29 18:53:37 +0100

    ✨ Add SonarQube integration and coverage report merging functionality

--- libs/core/jest.config.ts
+++ libs/core/jest.config.ts
@@ -6,5 +6,6 @@ export default {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: 'test-output/jest/coverage',
+  coverageReporters: ['cobertura', 'lcov'],
+  coverageDirectory: '../../coverage/libs/core',
 };
